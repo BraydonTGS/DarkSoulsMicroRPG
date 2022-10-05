6b741c8d002e2cb8b3d5a581001007cbe1fe0e99
Braydon Sutherland <BraydonSutherland@gmail.com>
2022-10-05 09:15:53 -0500

    starting the refactor on the world class

---
+++ DarkSoulsMicroRPG/World/Blighttown.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace DarkSoulsMicroRPG.World
+{
+    public class Blighttown
+    {
+        public Blighttown()
+        {
+        }
+    }
+}
+
---
+++ DarkSoulsMicroRPG/World/UndeadParish.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace DarkSoulsMicroRPG.World
+{
+    public class UndeadParish
+    {
+        public UndeadParish()
+        {
+        }
+    }
+}
+
